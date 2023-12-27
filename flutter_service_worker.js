073e671161399cb792f2c90ae30a5999823a1923
Fady Mamdouh <fadyarqam@gmail.com>
2023-12-27 16:19:09 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,8 +44,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "f569385572820c11c0abb590c3ce55ba
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1196b829ea93e8e9a4d1a774e2907a0b",
-"/": "1196b829ea93e8e9a4d1a774e2907a0b",
+"index.html": "53398cf3c17cf0e82522b6164f623faf",
+"/": "53398cf3c17cf0e82522b6164f623faf",
 "main.dart.js": "da4521ca67750fefe8aa51effc8b8cf6",
 "manifest.json": "37444b33de8e7995bb0a4deb91618dc0",
 "version.json": "8845e224ad1e9307205bd4bef17f3dff"};
